d08297532b658ecb4f192e9b9302513d0b72b6d1
Erik Yuntantyo <erik.yuntantyo@gmail.com>
2025-03-09 18:49:12 +0700

    upgrade sharp version to 0.33.5

--- index.js
+++ index.js
@@ -1,73 +1,84 @@
-const fs = require('fs');
-const sharp = require('sharp');
+const fs = require('fs')
+const sharp = require('sharp')
 
-function getDestination (req, file, cb) {
-  cb(null, '/dev/null');
-}
-function getFilename (req, file, cb) {
-  cb(null, 'filename.ext');
+function getDestination(req, file, cb) {
+  cb(null, '/dev/null')
 }
 
-function MulterSharp (opts) {
-  this.storage = opts.storage;
-  this.getDestination = (opts.destination || getDestination);
-  this.getFilename = (opts.filename || getFilename);
+function getFilename(req, file, cb) {
+  cb(null, 'filename.ext')
+}
+
+function MulterSharp(opts) {
+  this.storage = opts.storage
+  this.getDestination = opts.destination || getDestination
+  this.getFilename = opts.filename || getFilename
   this.size = opts.size || 512
   this.s3 = opts.s3
   this.folder = opts.folder
 }
 
-MulterSharp.prototype._handleFile = function _handleFile (req, file, cb) {
-  const self = this;
-  const resizer = sharp().resize(this.size, null, { withoutEnlargement: true });
+MulterSharp.prototype._handleFile = function _handleFile(req, file, cb) {
+  const resizer = sharp().resize({ width: this.size, fit: 'inside' })
 
-  this.getDestination(req, file, function (err, path) {
-    if (err) return cb(err);
+  this.getDestination(req, file, (err, path) => {
+    if (err) return cb(err)
 
-    self.getFilename(req, file, function (err, filename) {
-      if (err) return cb(err);
+    this.getFilename(req, file, (err, filename) => {
+      if (err) return cb(err)
 
-      if(self.s3){
-        const uploadParams = {ContentType: file.mimetype, ACL: 'public-read', Bucket: 'cdas-epayment', Key: '', Body: ''};
-        const fileStream = file.mimetype.includes('image') ? file.stream.pipe(resizer) : file.stream;
-        uploadParams.Body = fileStream;
-        uploadParams.Key = path+filename;
+      if (this.s3) {
+        const uploadParams = {
+          ContentType: file.mimetype,
+          ACL: 'public-read',
+          Bucket: 'cdas-epayment',
+          Key: '',
+          Body: '',
+        }
 
-        self.s3.upload (uploadParams, function (err, data) {
+        const fileStream = file.mimetype.includes('image')
+          ? file.stream.pipe(resizer)
+          : file.stream
+
+        uploadParams.Body = fileStream
+        uploadParams.Key = path + filename
+
+        this.s3.upload(uploadParams, (err, data) => {
           if (err) {
-            cb
-          } if (data) {
-            cb(null, {
-              filename,
-              path: data.Location,
-              size: 123
-            });
+            return cb(err)
           }
-        })
-        
-      } else {
-        
-        const outStream = fs.createWriteStream(path + filename);
 
-        file.mimetype.includes('image') ? file.stream.pipe(resizer).pipe(outStream) : file.stream.pipe(outStream);
-        
-        outStream.on('error', cb);
-        outStream.on('finish', function () {
+          cb(null, {
+            filename,
+            path: data.Location,
+            size: 123 // Consider using actual file size
+          })
+        })
+      } else {
+        const outStream = fs.createWriteStream(path + filename)
+        const processedStream = file.mimetype.includes('image')
+          ? file.stream.pipe(resizer)
+          : file.stream
+
+        processedStream.pipe(outStream)
+
+        outStream.on('error', cb)
+        outStream.on('finish', () => {
           cb(null, {
             filename,
             path: path + filename,
-            size: outStream.bytesWritten
-          });
-        });
+            size: outStream.bytesWritten,
+          })
+        })
       }
-    });
-  });
-};
+    })
+  })
+}
 
-MulterSharp.prototype._removeFile = function _removeFile (req, file, cb) {
-  fs.unlink(file.path, cb);
-};
+MulterSharp.prototype._removeFile = (req, file, cb) => {
+  fs.unlink(file.path, cb)
+}
 
-module.exports = function (opts) {
-  return new MulterSharp(opts);
-};
+module.exports = (opts) => {
+  return new MulterSharp(opts)
+}
